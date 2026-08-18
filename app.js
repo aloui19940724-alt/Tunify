9d6cfc82573e11728d0cef1761c63a11199af822
soufien <aloui19940724@gmail.com>
2026-08-18 23:20:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,32 @@ const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
 
 menuBtn.addEventListener("click", (e) => {
-  e.stopPropagation();
-  sidebar.classList.toggle("active");
+    e.stopPropagation();
+    sidebar.classList.toggle("active");
 });
 
 document.addEventListener("click", (e) => {
 
-  if (
-    sidebar.classList.contains("active") &&
-    !sidebar.contains(e.target) &&
-    e.target !== menuBtn
-  ) {
-    sidebar.classList.remove("active");
-  }
+    if (
+        sidebar.classList.contains("active") &&
+        !sidebar.contains(e.target) &&
+        e.target !== menuBtn
+    ) {
+        sidebar.classList.remove("active");
+    }
 
 });
+
+async function goToProfile() {
+
+    const {
+        data: { session }
+    } = await supabaseClient.auth.getSession();
+
+    if (session) {
+        window.location.href = "profile.html";
+    } else {
+        window.location.href = "login.html";
+    }
+
+}
