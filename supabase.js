e285a27da29ba22c2d76c596c96f3502db74e3ac
soufien <aloui19940724@gmail.com>
2026-08-18 22:53:40 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 const SUPABASE_URL = "https://qbrbmkuazanubqmhjhlh.supabase.co";
 
-const SUPABASE_ANON_KEY =
-"ضع_مفتاح_Anon_هنا";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFicmJta3VhemFudWJxbWhqaGxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwODI2MDgsImV4cCI6MjEwMjY1ODYwOH0.Wwv9XfyYHH9jm5yhtRjlpZaIV0wUJr9rPt5CCj7CyBE";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
