ef0cb1d0e01c6bf586c18cb5aa40033dbde6501c
soufien <aloui19940724@gmail.com>
2026-08-18 23:18:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
 
-menuBtn.addEventListener("click", () => {
-sidebar.classList.toggle("active");
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  sidebar.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+
+  if (
+    sidebar.classList.contains("active") &&
+    !sidebar.contains(e.target) &&
+    e.target !== menuBtn
+  ) {
+    sidebar.classList.remove("active");
+  }
+
 });
